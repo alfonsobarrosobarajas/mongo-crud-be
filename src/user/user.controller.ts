836f15f5b73b67c1e6bb10247df680e769dcb969
Alfonso Jose Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2021-07-15 20:17:04 -0600

    Primer commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Logger } from '@nestjs/common';
+
+// Indica la ruta de acceso al 'endpoint'
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  /*
+  @Post()
+  async create(@Body('name') name: string, @Body('surname') surname: string, @Body('points') points: string) {
+    return this.userService.create(name, surname, points);
+  }
+*/
+
+  @Post()
+  async create(@Body() user: User) {
+    const name = user.name;
+    const surname = user.surname;
+    const points = user.points;
+
+    Logger.log(user);
+
+    this.userService.create(name, surname, points);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('surname') surname: string,
+    @Body('points') points: number,
+  ) {
+    return this.userService.update(id, name, surname, points);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
+}
